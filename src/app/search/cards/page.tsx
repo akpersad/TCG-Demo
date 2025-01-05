7a8edc1ff907cec117fc63a0f0e5adef5496ee6e
Andrew Persad <akpersad@gmail.com>
2025-01-05 00:16:54 -0500

    Start of search pages

---
+++ src/app/search/cards/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const CardsPage = () => {
+  return (
+    <div>
+      <h1>Search Cards</h1>
+      {/* Add your search functionality and card display components here */}
+    </div>
+  );
+};
+
+export default CardsPage;
