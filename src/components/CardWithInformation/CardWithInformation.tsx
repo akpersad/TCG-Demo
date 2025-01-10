166bec123cc5e07e637d05889f609ebc7bd6813d
Andrew Persad <akpersad@gmail.com>
2025-01-10 14:31:52 -0500

    Add container around individual cards to allow for toggle of heart action

---
+++ src/components/CardWithInformation/CardWithInformation.tsx
@@ -0,0 +1,51 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import { PokemonTCG } from 'pokemon-tcg-sdk-typescript';
+import { useState } from 'react';
+import Heart from '../../../public/assets/heart.svg';
+import HeartOutline from '../../../public/assets/heart-outline.svg';
+import styles from './CardWithInformation.module.scss';
+
+interface Props {
+  card: PokemonTCG.Card;
+  isSignedIn?: boolean;
+}
+
+const CardWithInformation = ({ card, isSignedIn }: Props) => {
+  const [showSavedToDB, setShowSavedToDB] = useState<boolean>(false);
+  return (
+    <div
+      className={`flex flex-col flex-wrap gap-2 relative ${styles.displayHeader}`}
+    >
+      {isSignedIn && (
+        <button
+          className={styles.saveButton}
+          onClick={() => {
+            setShowSavedToDB(!showSavedToDB);
+            console.log(`save card ${card.id}`);
+          }}
+        >
+          <Image
+            src={showSavedToDB ? HeartOutline : Heart}
+            alt={`Save ${card.name} to collection`}
+            title='Save Card to Collection'
+            height={40}
+            width={40}
+          />
+        </button>
+      )}
+      <Link href={`/search/cards/${card.id}`}>
+        <h2 className='capitalize text-lg font-semibold'>{card.name}</h2>
+        <Image
+          src={card.images.small}
+          alt={card.name}
+          width={300}
+          height={500}
+          className={styles.card}
+        />
+      </Link>
+    </div>
+  );
+};
+
+export default CardWithInformation;
--- src/components/DisplayCards/DisplayCards.tsx
+++ src/components/DisplayCards/DisplayCards.tsx
@@ -1,11 +1,6 @@
-import Image from 'next/image';
 import { PokemonTCG } from 'pokemon-tcg-sdk-typescript';
-import styles from './DisplayCards.module.scss';
-import Link from 'next/link';
-import Heart from '../../../public/assets/heart.svg';
-import HeartOutline from '../../../public/assets/heart-outline.svg';
-import { useState } from 'react';
 import LoadingOverlay from '@/components/LoadingOverlay/LoadingOverlay';
+import CardWithInformation from '../CardWithInformation/CardWithInformation';
 
 interface DisplayCardsProps {
   displayCards?: PokemonTCG.Card[];
@@ -18,44 +13,16 @@ const DisplayCards = ({
   isSignedIn,
   dataLoading,
 }: DisplayCardsProps) => {
-  const [showSavedToDB, setShowSavedToDB] = useState<boolean>(false);
   return (
     <div className='relative'>
       {dataLoading && <LoadingOverlay />}
       <div className='grid grid-cols-4 gap-5'>
         {displayCards?.map((card, index) => (
-          <div
-            key={index}
-            className={`flex flex-col flex-wrap gap-2 relative ${styles.displayHeader}`}
-          >
-            {isSignedIn && (
-              <button
-                className={styles.saveButton}
-                onClick={() => {
-                  setShowSavedToDB(!showSavedToDB);
-                  console.log(`save card ${card.id}`);
-                }}
-              >
-                <Image
-                  src={showSavedToDB ? HeartOutline : Heart}
-                  alt={`Save ${card.name} to collection`}
-                  title='Save Card to Collection'
-                  height={40}
-                  width={40}
-                />
-              </button>
-            )}
-            <Link href={`/search/cards/${card.id}`}>
-              <h2 className='capitalize text-lg font-semibold'>{card.name}</h2>
-              <Image
-                src={card.images.small}
-                alt={card.name}
-                width={300}
-                height={500}
-                className={styles.card}
-              />
-            </Link>
-          </div>
+          <CardWithInformation
+            key={`display-card-${card.id}-${index}`}
+            card={card}
+            isSignedIn={isSignedIn}
+          />
         ))}
       </div>
     </div>
