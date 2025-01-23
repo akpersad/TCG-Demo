d209f36388008623836426ead7c61d50487592b8
Andrew Persad <akpersad@gmail.com>
2025-01-23 10:39:18 -0500

    Add transparency to background

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 const Footer = () => {
   const currentYear = new Date().getFullYear();
   return (
-    <footer className='bg-gray-800 text-white py-4'>
+    <footer className='bg-transparent text-white py-4'>
       <div className='container mx-auto text-center'>
         <p className='text-base'>
           Made By{' '}
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -18,14 +18,14 @@ const Header = () => {
     'block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent';
 
   return (
-    <nav className='sticky border-gray-200 bg-gray-50 dark:bg-gray-800'>
+    <nav className='sticky border-gray-200 bg-transparent'>
       <div className='max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4 '>
         <Link
           href='/'
           className='flex items-center space-x-3 rtl:space-x-reverse'
         >
           <span className='self-center text-2xl font-semibold whitespace-nowrap dark:text-white'>
-            POKEMON TCG DEMO
+            POKÉ COLLECTOR
           </span>
         </Link>
         <button
@@ -56,7 +56,7 @@ const Header = () => {
           className='hidden w-full md:block md:w-auto'
           id='navbar-default bg-gray-50 dark:bg-gray-800'
         >
-          <ul className='font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 dark:bg-gray-800 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white'>
+          <ul className='font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-transparent md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 '>
             <li>
               <Link
                 href='/'
