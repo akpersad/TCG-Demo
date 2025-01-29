2d57b0dbc4250dadc2778c24894a2a5fcdac0314
Andrew Persad <akpersad@gmail.com>
2025-01-29 10:36:51 -0500

    Sets mobile styling

--- src/components/SetsContainer/SetsContainer.tsx
+++ src/components/SetsContainer/SetsContainer.tsx
@@ -9,11 +9,13 @@ type Props = {
 
 const SetsContainer = ({ sets }: Props) => {
   return (
-    <div className='container mx-auto my-8'>
+    <div className='container mx-auto sm:my-8 px-4'>
       {sets.map((group) => (
         <div key={group.series} className='rounded-lg mb-8'>
-          <h2 className='text-3xl font-semibold p-4'>{group.series}</h2>
-          <div className='grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-4'>
+          <h2 className='text-xl sm:text-3xl font-semibold py-2 sm:py-4'>
+            {group.series}
+          </h2>
+          <div className='grid grid-cols-2 gap-4 lg:grid-cols-4'>
             {group.sets.map((set) => (
               <Link
                 key={set.id}
@@ -25,18 +27,20 @@ const SetsContainer = ({ sets }: Props) => {
                   alt={set.name}
                   width={500}
                   height={500}
-                  className='w-full h-48 object-contain'
+                  className='w-full sm:h-48 object-contain'
                 />
                 <div className='p-4'>
-                  <h3 className='text-xl font-semibold'>{set.name}</h3>
-                  <p>
+                  <h3 className='text-base sm:text-xl font-semibold'>
+                    {set.name}
+                  </h3>
+                  <p className='text-sm sm:text-base'>
                     {new Date(set.releaseDate).toLocaleDateString('en-US', {
                       month: 'long',
                       day: 'numeric',
                       year: 'numeric',
                     })}
                   </p>
-                  <p>{set.total} cards</p>
+                  <p className='text-sm sm:text-base'>{set.total} cards</p>
                 </div>
               </Link>
             ))}
